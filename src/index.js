b196ed786a464227f8696c36735c4df92d05355a
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-22 15:43:27 -0300

    reading resources

--- src/index.js
+++ src/index.js
@@ -17,12 +17,52 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch((e) =>{
+        res.status(404).send(e)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send(user)
+        }
+        return res.send(user)
+    }).catch((e) =>{
+        res.status(404).send(e)
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((e) =>{
+        res.status(404).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send(task)
+        }
+        return res.send(task)
+    }).catch((e) =>{
+        res.status(404).send(e)
     })
 })
 
