c8620b8eb9f0985f7cc60bf9395e9a0891359238
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-27 23:02:34 -0300

    email unique

--- src/models/user.js
+++ src/models/user.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         validate(value) {
             if (!validator.isEmail(value)) {
