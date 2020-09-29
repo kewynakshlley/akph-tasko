6ef24b93e69ddbcb823bcedb9aacca4469167b0f
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-29 20:58:42 -0300

    user logout feature

--- src/routers/user.js
+++ src/routers/user.js
@@ -82,6 +82,19 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
 
 
 module.exports = router
