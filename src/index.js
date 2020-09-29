9745ab01e6372b96ffff6e4ad83986f74d9b7253
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-29 00:34:44 -0300

    accepting auth tokens and adding express middleware

--- src/index.js
+++ src/index.js
@@ -2,24 +2,14 @@ require('./db/mongoose')
 const express = require('express')
 const taskRouter = require('./routers/task')
 const userRouter = require('./routers/user')
-const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const myFunc = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismycourse', {expiresIn: '5 second'})
-    console.log(token)
-    console.log(jwt.verify(token, 'thisismycourse'))
-}
-
-myFunc()
