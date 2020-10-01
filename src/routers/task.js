c6776b11b10f6c0a887c806022cf86dd82f57693
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-30 23:41:47 -0300

    user can just see tasks that they've created (get one task)

--- src/routers/task.js
+++ src/routers/task.js
@@ -26,11 +26,12 @@ router.get('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
         if (!task) {
             return res.status(404).send()
         }
