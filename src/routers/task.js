f6ef622c56689b323825d38797447e5196e22481
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-10-02 01:26:14 -0300

    tasks sorting and pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,14 +19,26 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
+
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split('_')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try {
         //const tasks = await Task.find({ owner: req.user._id})
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.status(200).send(req.user.tasks)
     } catch (e) {
