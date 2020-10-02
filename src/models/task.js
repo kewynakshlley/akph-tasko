0ee7abbcf26fc6d97adff005b14e0945ca78bef9
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-10-01 21:55:11 -0300

    timestamps of users and tasks

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -14,6 +15,10 @@ const Task = mongoose.model('Task', {
         type: mongoose.Schema.Types.ObjectId,
         required: true
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
